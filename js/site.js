913880ee32c97acdda00d75c27a92f6a342adf50
BrandonLosch <113945271+BrandonLosch@users.noreply.github.com>
2022-10-12 12:52:42 -0400

    inital commit

--- js/site.js
+++ js/site.js
@@ -1,15 +1,34 @@
-function displayMessage() {
 
-    let msg = document.getElementById("message").value;
-    //alert(msg);
+function getValue() {
+
+    //hides the results alert
+    document.getElementById("alert").classList.add('invisible');
+
+    //get the value
+    let userString = document.getElementById("userString").value;
+
+    //Reverse the user input
+    let revString = reverseString(userString);
+
+    //display the user input
+    displayString(revString); 
     
-    Swal.fire(
-    {
-        backdrop: false,
-        title: 'App Name',
-        text: msg
-    }
+}
 
-    )
+
+//Reverse the string
+function reverseString(userString) {
+    let usrArray = userString.split('');
+    let revArray = usrArray.reverse();
+    let revString = revArray.join('');
+
+    return revString;
+
+}
+
+//Display the string
+function displayString (revString) {
+    document.getElementById('msg').innerHTML = `Your String Reversed is:<strong>${revString}</strong>`;
+    document.getElementById("alert").classList.remove('invisible');
 }
 
