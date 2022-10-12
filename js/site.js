771e5c7719ab19d55762a909f1a01774681a6d74
BrandonLosch <113945271+BrandonLosch@users.noreply.github.com>
2022-10-12 12:12:18 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,15 @@
+function displayMessage() {
+
+    let msg = document.getElementById("message").value;
+    //alert(msg);
+    
+    Swal.fire(
+    {
+        backdrop: false,
+        title: 'App Name',
+        text: msg
+    }
+
+    )
+}
+
